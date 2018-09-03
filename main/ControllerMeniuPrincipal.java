f7a75ae6dcdb809f3d14238f705ebbc059618900
Paul-Cristian Vasile <vasilepaul97@gmail.com>
2018-09-03 08:23:26 +0300

    Citire XML si navigare prin meniu

--- main/ControllerMeniuPrincipal.java
+++ main/ControllerMeniuPrincipal.java
@@ -4,14 +4,13 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
+import java.io.*;
 
 import static javax.swing.JOptionPane.showMessageDialog;
 
@@ -36,8 +35,14 @@ public class ControllerMeniuPrincipal {
             butonValidareSemnatura.setVisible(false);
             statusFisier.setText("Fisierul incarcat este in format XML. Functia de validare a semnaturii nu se poate folosi!");
 
-        }else {
+        }else if(f.getName().contains(".p7b") || f.getName().contains(".p7s") || f.getName().contains(".p7m")){
             fisierSemnat = f;
+            //extragerea payloadului din mesajul semnat
+            try {
+                FileInputStream in = new FileInputStream(f);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
             butonValidareSemnatura.setVisible(true);
         }
         labelFisier.setText(f.getName());
@@ -47,7 +52,15 @@ public class ControllerMeniuPrincipal {
     @FXML
     private void openCitireXML(){
 
-        ReadXMLFile obiectReadXML = new ReadXMLFile();
+        if(ControllerMeniuPrincipal.fisierXML == null){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Oroare!!!");
+            alert.setHeaderText("Nu a fost incarcat un fisier!");
+            alert.setContentText("Pentru Citirea XML-ului trebuie incarcat un fisier în format .xml");
+            alert.showAndWait();
+            return;
+        }
+
         Parent root = null;
         try {
             root = FXMLLoader.load(getClass().getResource("Interfata Citire XML.fxml"));
@@ -57,5 +70,11 @@ public class ControllerMeniuPrincipal {
             Stage curenta = (Stage) butonCitireXML.getScene().getWindow();
             curenta.setTitle("Test");
             curenta.setScene(new Scene(root, 900, 700));
+    }
+
+    @FXML
+    private void openValidareSemnatura(){
+        //showMessageDialog(null,"Test");
+        System.out.println("Modul de validare semnatura");
     }
 }
--- main/ReadXMLFile.java
+++ main/ReadXMLFile.java
@@ -1,12 +1,19 @@
 package main;
 
+import java.awt.event.MouseEvent;
+import java.beans.EventHandler;
 import java.io.File;
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import javax.xml.XMLConstants;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -18,11 +25,14 @@ import javafx.scene.control.RadioButton;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Font;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import main.ControllerMeniuPrincipal;
 import org.w3c.dom.Document;
 import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import static javax.swing.JOptionPane.showMessageDialog;
 
@@ -38,6 +48,9 @@ public class ReadXMLFile {
 
     @FXML
     private void goBack(){
+        ControllerMeniuPrincipal.fisierXML = null;
+        ControllerMeniuPrincipal.fisierSemnat = null;
+
         Parent root = null;
         try {
             root = FXMLLoader.load(getClass().getResource("Meniu Principal.fxml"));
@@ -52,7 +65,17 @@ public class ReadXMLFile {
     @FXML
     private void citireXML(){
 
-        labelValidareXSD.setText("Aici urmeaza validare XSD");
+        validareXSD();
+
+        if(optiune.isSelected()){
+            Validare = true;
+        }
+
+        /*if(Validare.booleanValue() == false) {
+            labelValidareXSD.setText("Doar citire");
+        }else{
+            labelValidareXSD.setText("Citire si validare cu datele din testul de calcule");
+        }*/
 
         if(ControllerMeniuPrincipal.fisierXML == null)
         {
@@ -63,10 +86,6 @@ public class ReadXMLFile {
         start.setVisible(false);
         optiune.setVisible(false);
         mesajOptiune.setVisible(false);
-
-        if(optiune.isSelected()){
-            Validare = true;
-        }
 
         try {
 
@@ -91,6 +110,100 @@ public class ReadXMLFile {
 
     private void validareXSD(){
 
+        boolean ok = false;
+
+        try {
+            SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+
+            File f = new File("/Users/paul-cristianvasile/Desktop/ExportValidator/main/xmlUzual.xsd");
+
+            Schema schema = factory.newSchema(f);
+
+            Validator validator = schema.newValidator();
+
+            validator.validate(new StreamSource(ControllerMeniuPrincipal.fisierXML));
+            labelValidareXSD.setText("Validat automat cu XSD-ul de AMEF Uzual!");
+            ok = true;
+
+        } catch (IOException e){
+            System.out.println(e.getMessage());
+            //xmlUzualLabel.setText(e.getMessage());
+        }catch(SAXException e1){
+            //xmlUzualLabel.setText(e1.getMessage());
+            System.out.println("SAX Exception: "+e1.getMessage());
+        }
+
+        if(ok == false){
+            try {
+                SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+
+                File f = new File("/Users/paul-cristianvasile/Desktop/ExportValidator/main/xmlPR.xsd");
+
+                Schema schema = factory.newSchema(f);
+
+                Validator validator = schema.newValidator();
+
+                validator.validate(new StreamSource(ControllerMeniuPrincipal.fisierXML));
+                labelValidareXSD.setText("Validat automat cu XSD-ul pentru Perioada de raportare (mesaj registru)!");
+                ok = true;
+
+            } catch (IOException e){
+                System.out.println(e.getMessage());
+                //xmlPRLabel.setText(e.getMessage());
+            }catch(SAXException e1){
+                //xmlPRLabel.setText(e1.getMessage());
+                System.out.println("SAX Exception: "+e1.getMessage());
+            }
+
+            if(ok == false){
+                try {
+                    SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+
+                    File f = new File("/Users/paul-cristianvasile/Desktop/ExportValidator/main/xmlSV.xsd");
+
+                    Schema schema = factory.newSchema(f);
+
+                    Validator validator = schema.newValidator();
+
+                    validator.validate(new StreamSource(ControllerMeniuPrincipal.fisierXML));
+                    labelValidareXSD.setText("Validat cu XSD-ul pentru AMEF Schimb Valutar!");
+                    ok = true;
+
+                } catch (IOException e){
+                    System.out.println(e.getMessage());
+                    //xmlSVLabel.setText(e.getMessage());
+                }catch(SAXException e1){
+                    //xmlSVLabel.setText(e1.getMessage());
+                    System.out.println("SAX Exception: "+e1.getMessage());
+                }
+
+                if(ok == false){
+                    try {
+                        SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+
+                        File f = new File("/Users/paul-cristianvasile/Desktop/ExportValidator/main/xmlTaxi.xsd");
+
+                        Schema schema = factory.newSchema(f);
+
+                        Validator validator = schema.newValidator();
+
+                        validator.validate(new StreamSource(ControllerMeniuPrincipal.fisierXML));
+                        labelValidareXSD.setText("Validat!");
+                        ok = true;
+
+                    } catch (IOException e){
+                        System.out.println(e.getMessage());
+                        //xmlTaxiLabel.setText(e.getMessage());
+                    }catch(SAXException e1){
+                        //xmlTaxiLabel.setText(e1.getMessage());
+                        System.out.println("SAX Exception: "+e1.getMessage());
+                    }
+                }
+                }
+            }
+            if(ok == false){
+                labelValidareXSD.setText("Atentie! Eroare la validarea fisierului cu XSD-ul ANAF!");
+            }
     }
 
     private void printNote(NodeList nodeList) {
@@ -104,6 +217,12 @@ public class ReadXMLFile {
 
                 // get node name and value
                 System.out.println("\nNumele Nodului =" + tempNode.getNodeName() + " [OPEN]");
+
+                if(tempNode.getNodeName().equals("mReg")) {
+                    Label l = new Label("Informații referitoare la perioada de raportare");
+                    l.setFont(new Font("Arial", 22));
+                    grid.add(l, 0, 0);
+                }
 
                 if(tempNode.getNodeName().equals("msj")) {
                     Label l = new Label("Sectiune referitoare la mesajul semnat");
@@ -184,18 +303,50 @@ public class ReadXMLFile {
 
         switch (n.getNodeName()){
 
+            case "cif":{
+                if(Validare){
+                    grid.addColumn(0,new Label("C.I.F.: " + n.getNodeValue() + " De verificat manual!"));
+                }
+                else {
+                    grid.addColumn(0, new Label("C.I.F.: " + n.getNodeValue()));
+                }
+                break;
+            }
+
+            case "nrRapI":{
+                grid.addColumn(0,new Label("Numarul primului Z raportat: " + n.getNodeValue()));
+                break;
+            }
+
+            case "nrRapF":{
+                grid.addColumn(0,new Label("Numarul ultimului Z raportat: " + n.getNodeValue()));
+                break;
+            }
+
+            case "tip_amef":{
+                String tip = "nedeclarat";
+                switch(n.getNodeValue()){
+                    case "U":{ tip = "Uzual"; break;}
+                    case "T":{ tip = "Taximetrie"; break;}
+                    case "S":{ tip = "Schimb Valutar"; break;}
+                    case "A":{ tip = "Magazin aeroporturi"; break;}
+                }
+                grid.addColumn(0,new Label("Tipul de AMEF: " + tip));
+                break;
+            }
+
             case "idM":{
                 try {
 
                     grid.addColumn(0,new Label("Seria Fiscala este : " + n.getNodeValue().substring(0, 10)));
-                    if(Validare){grid.addRow(1,new Label("De verificat manual"));}
+
                     SimpleDateFormat parser = new SimpleDateFormat("yyyymmddhhmmss");
                     Date date = parser.parse(n.getNodeValue().substring(10, 24));
                     SimpleDateFormat formatter = new SimpleDateFormat("dd-mm-yyyy HH:mm:ss ");
                     grid.addColumn(0,new Label("Data emiterii raportului este: " + formatter.format(date)));
-                    if(Validare){grid.addRow(2,new Label("De verificat manual"));}
+
+
                     grid.addColumn(0,new Label("Numarul raportului Z verificat este: " + n.getNodeValue().substring(24,28)));
-                    if(Validare){grid.addRow(3,new Label("De verificat manual"));}
 
                 }catch (Exception e){
                     System.out.println(e);
